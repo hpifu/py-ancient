d5c83030701fe342b206b0e179572b39c2844b11
hatlonely <hatlonely@gmail.com>
2019-09-17 20:21:26 +0800

    update

--- gwx.babihu.com/link.py
+++ gwx.babihu.com/link.py
@@ -3,6 +3,7 @@
 import requests
 import sys
 from pyquery import PyQuery as pq
+import argparse
 
 www = "http://gwx.babihu.com"
 
@@ -22,7 +23,7 @@ def extractLink(url):
             d = pq(res.text)
             pairs = []
             for a in d("a").items():
-                pairs.append((a.text(), a.attr("href")))
+                pairs.append(a.attr("href"))
             return pairs
         except Exception as e:
             count += 1
@@ -30,31 +31,47 @@ def extractLink(url):
                 return []
 
 
-def travel(filename="stdout"):
+def travel(filename="stdout", limit=0):
     hrefs = [www]
     hrefset = set(hrefs)
     if filename == "stdout":
         fp = sys.stdout
     else:
         fp = open(filename, "w")
+    count = 0
     while hrefs:
         url = hrefs[-1]
-        for pair in extractLink(url):
-            text, href = pair
+        for href in extractLink(url):
             if href in hrefset:
                 continue
             hrefset.add(href)
             if href.startswith("/article/view"):
-                fp.write("{}\t{}\t{}\n".format(text, www+href, url))
+                fp.write("{}\n".format(www+href))
+                count += 1
+                if limit != 0 and count >= limit:
+                    break
             elif href.startswith("/"):
                 hrefs.append(www+href)
+        if limit != 0 and count >= limit:
+            break
         hrefs.pop()
         fp.flush()
     fp.close()
 
 
 def main():
-    travel()
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description="""Example:
+    python3 link.py --limit 1
+""",
+    )
+    parser.add_argument("-l", "--limit", default=0,
+                        type=int, help="limit links")
+    parser.add_argument("-o", "--output", default="stdout",
+                        help="output filename")
+    args = parser.parse_args()
+    travel(args.output, args.limit)
 
 
 if __name__ == "__main__":
