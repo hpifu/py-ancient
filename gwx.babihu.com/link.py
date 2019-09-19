1dc90a6e57088776e79c86a0d249f14262255b1b
hatlonely <hatlonely@gmail.com>
2019-09-19 18:48:30 +0800

    update

--- gwx.babihu.com/link.py
+++ gwx.babihu.com/link.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
+import argparse
 import requests
 import sys
+import json
+import os
+import pickle
 from pyquery import PyQuery as pq
-import argparse
 
 www = "http://gwx.babihu.com"
 
@@ -21,42 +24,64 @@ def extractLink(url):
                 }
             )
             d = pq(res.text)
-            pairs = []
+            hrefs = []
             for a in d("a").items():
-                pairs.append(a.attr("href"))
-            return pairs
+                hrefs.append(a.attr("href"))
+            return hrefs
         except Exception as e:
             count += 1
             if count == 3:
                 return []
 
 
+def dump(obj, filename):
+    fp = open(filename, "wb")
+    pickle.dump(obj, fp)
+    fp.close()
+
+
+def load(filename, default):
+    if not os.path.exists(filename):
+        return default
+    fp = open(filename, "rb")
+    obj = pickle.load(fp)
+    fp.close()
+    return obj
+
+
 def travel(filename="stdout", limit=0):
-    hrefs = [www]
-    hrefset = set(hrefs)
+    queue = load(".queue", [www])
+    history = load(".history", set())
     if filename == "stdout":
         fp = sys.stdout
     else:
         fp = open(filename, "w")
     count = 0
-    while hrefs:
-        url = hrefs[-1]
+    num = 0
+    while queue:
+        url = queue[-1]
         for href in extractLink(url):
-            if href in hrefset:
+            if href in history:
                 continue
-            hrefset.add(href)
+            history.add(href)
             if href.startswith("/article/view"):
                 fp.write("{}\n".format(www+href))
                 count += 1
                 if limit != 0 and count >= limit:
                     break
             elif href.startswith("/"):
-                hrefs.append(www+href)
+                queue.append(www+href)
         if limit != 0 and count >= limit:
             break
-        hrefs.pop()
+        num += 1
+        if num % 100 == 0:
+            dump(queue, ".queue")
+            dump(history, ".history")
+        queue.pop()
         fp.flush()
     fp.close()
+    dump(queue, ".queue")
+    dump(history, ".history")
 
 
 def main():
